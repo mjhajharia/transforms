98537c0879872be23bdb1f94d3872a94ecd84e7d
Meenal Jhajharia <meenal@mjhajharia.com>
2022-07-19 13:18:19 -0400

    maintenance (#32)

---
+++ simplex.py
@@ -0,0 +1,21 @@
+import os
+from tqdm import tqdm
+os.chdir('transforms')
+
+import sys
+sys.path.insert(1, 'utils')
+
+from sample import sample
+
+parameters = [{'alpha': [0.1]*10, 'N': 10}, {'alpha': [0.1]*100, 'N': 100}, {'alpha': [0.1]*1000, 'N': 1000},
+{'alpha': [1]*10, 'N': 10}, {'alpha': [1]*100, 'N': 100}, {'alpha': [1]*1000, 'N': 1000},
+{'alpha': [1]*10, 'N': 10}, {'alpha': [1]*100, 'N': 100}, {'alpha': [1]*1000, 'N': 1000}]
+
+for transform in tqdm(transforms):
+    for params in tqdm(parameters):
+        sample(transform_category='simplex', transform=transforms, 
+            evaluating_model='dirichlet_symmetric', parameters=[params], output_file='/mnt/ceph/users/mjhajaria/sampling_results/stan_1.json', 
+            auto_eval_all_params=False, n_iter = 1000, n_chains = 4, n_repeat = 100, 
+                            show_progress = True, resample=True, return_idata=False)
+
+
--- utils/dirichlet_symmetric.py
+++ utils/dirichlet_symmetric.py
@@ -4,52 +4,30 @@ import matplotlib.pyplot as plt
 from rmse import rmse_leapfrog
 from sample import sample
 
+def get_dirichlet_symmetric_rmse(transforms, transform_category, parameters, fig_name, n_repeat=1, n_iter=1000, 
+                                n_chains=4, show_progress=True, resample=False):
+    
+    plt.rcParams["figure.figsize"] = [20,10]
+    plt.rcParams['figure.dpi'] = 300
+    fig, axes = plt.subplots(3,3)
+    fig.supxlabel('Cumulative Leapfrog Steps')
+    fig.supylabel('Root Mean Squared Error')
 
-def get_dirichlet_symmetric_rmse(
-    transforms,
-    transform_category,
-    parameters,
-    fig_name,
-    n_repeat=1,
-    n_iter=1000,
-    n_chains=4,
-    show_progress=True,
-    resample=False,
-):
+    for ax, params in zip(axes.flatten() if len(parameters)>1 else [axes],  parameters):
+        for transform in transforms:            
+            idata = sample(transform_category=transform_category, transform=transform, 
+                evaluating_model='dirichlet_symmetric', parameters=[params], output_file=None, 
+                auto_eval_all_params=False, n_iter = n_iter, n_chains = n_chains, n_repeat = n_repeat, 
+                                        show_progress = show_progress, resample=resample)
+            alpha = params['alpha']
+            N = params['N']
+            true_x = [a/sum(alpha) for a in alpha]
+            x, y = rmse_leapfrog(idata=idata, true_var=true_x, var_name='x', var_dim=0)
+            ax.plot(x,y, label = str(transform))
 
-    plt.rcParams["figure.figsize"] = [20, 10]
-    plt.rcParams["figure.dpi"] = 300
-    fig, axes = plt.subplots(len(parameters))
-    fig.supxlabel("Cumulative Leapfrog Steps")
-    fig.supylabel("Root Mean Squared Error")
-
-    for ax, params in zip(
-        axes.flatten() if len(parameters) > 1 else [axes], parameters
-    ):
-        for transform in transforms:
-            idata = sample(
-                transform_category=transform_category,
-                transform=transform,
-                evaluating_model="dirichlet_symmetric",
-                parameters=[params],
-                output_file=None,
-                auto_eval_all_params=False,
-                n_iter=n_iter,
-                n_chains=n_chains,
-                n_repeat=n_repeat,
-                show_progress=show_progress,
-                resample=resample,
-            )
-            alpha = params["alpha"]
-            N = params["N"]
-            true_x = [a / sum(alpha) for a in alpha]
-            x, y = rmse_leapfrog(idata=idata, true_var=true_x, var_name="x", var_dim=0)
-            ax.plot(x, y, label=str(transform))
-
-        ax.set_title(f"alpha={alpha[0]}, N = {N}")
-    fig.legend(labels=transforms, bbox_to_anchor=(0.6, -0.05))
-    fig.savefig(f"figures/{fig_name}", dpi=300)
-
+        ax.set_title(f'alpha={alpha[0]}, N = {N}')
+    fig.legend(labels=transforms,bbox_to_anchor = (0.6, -0.05));
+    fig.savefig(f'figures/{fig_name}', dpi=300)
 
 def create_param_map():
     alphas = [0.1, 1, 10]
