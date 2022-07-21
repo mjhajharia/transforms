95982c87f41e1e1699a0ce731ffe78fda0e557f3
mjhajharia <meenal@mjhajharia.com>
2022-07-21 01:46:15 -0400

    whitespace adjustment

--- utils/sample.py
+++ utils/sample.py
@@ -129,7 +129,7 @@ def sample(
 
             filename = f'{output_dir}/sampling_results/{transform_category}/{transform}/{evaluating_model}/{param_map[tuple(list(params.values())[0])]}_{n_repeat}.nc'
             idata.to_netcdf(filename)
-	    with open(f'{output_dir}/sampling_results/{transform_category}/{transform}/{evaluating_model}/time_{param_map[tuple(list(params.values())[0])]}_{n_repeat}.txt', 'w') as f:
+        with open(f'{output_dir}/sampling_results/{transform_category}/{transform}/{evaluating_model}/time_{param_map[tuple(list(params.values())[0])]}_{n_repeat}.txt', 'w') as f:
 	        f.write(str(time.time() - start_time))
         if return_idata==True:
             return idata
