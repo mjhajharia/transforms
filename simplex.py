6f48338b4ae0f2c45d3946f424a1139f8756c6d9
mjhajharia <meenal@mjhajharia.com>
2022-07-21 22:54:49 -0700

    add plots

--- simplex.py
+++ simplex.py
@@ -21,6 +21,6 @@ N = len(alpha)
 
 sample(transform_category='simplex', transform=args.transform, evaluating_model='dirichlet_symmetric', 
 parameters={'alpha':alpha, 'N':N}, 
-            auto_eval_all_params=False, n_iter = 1000, n_chains = 4, n_repeat = 2, 
+            auto_eval_all_params=False, n_iter = 1000, n_chains = 4, n_repeat = 100, 
                             show_progress = True, resample=True, return_idata=False)
                             
