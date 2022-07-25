292323ea9b5a1b6e7db30b885094b6373004019c
mjhajharia <meenal@mjhajharia.com>
2022-07-25 13:36:01 -0700

    -

--- plot.py
+++ plot.py
@@ -28,11 +28,11 @@ fig.supylabel('Root Mean Squared Error')
 for ax, params in zip(axes.flatten() if len(parameters)>1 else [axes],  parameters):
     for transform in transforms:
         idata = sample(transform_category=transform_category, transform=transform,
-            evaluating_model='dirichlet_symmetric', parameters=parameters,
+            evaluating_model='dirichlet_symmetric', parameters=[params],
             auto_eval_all_params=False, n_iter = 1000,  n_chains = 4, n_repeat=100,
                                     show_progress = True, resample=False,return_idata=True)
-        alpha = parameters[0]['alpha']
-        N = parameters[0]['N']
+        alpha = params['alpha']
+        N = params['N']
         true_x = [a/sum(alpha) for a in alpha]
         x, y = rmse_leapfrog(idata=idata, true_var=true_x, var_name='x', var_dim=0)
         ax.plot(x,y, label = str(transform))
