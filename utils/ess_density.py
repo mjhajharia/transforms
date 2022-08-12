07c04726f190404729745fd201680f4769c79c3e
mjhajharia <meenal@mjhajharia.com>
2022-08-12 05:38:42 -0700

    ess pdf plot

--- utils/ess_density.py
+++ utils/ess_density.py
@@ -31,6 +31,6 @@ for ax, params in zip(axes.flatten() if len(parameters)>1 else [axes],  paramete
     ax.set_title(f'alpha = {params["alpha"][0]}, N = {params["N"]}')
 ax.axes.yaxis.set_ticklabels([])
 fig.supxlabel('ESS/Leapfrog')
-fig.supylabel('Density')
+fig.supylabel('Probability Density Function')
 plt.legend()
 plt.savefig('figures/simplex/ess_density.png', dpi=300)
